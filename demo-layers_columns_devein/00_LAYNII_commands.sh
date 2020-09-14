3c4302dec81470b9d19da28d8ae57090f56a06bc
Omer Faruk Gulban <farukgulban@gmail.com>
2020-09-14 17:50:53 +0200

    Update

--- demo-layers_columns_devein/00_LAYNII_commands.sh
+++ demo-layers_columns_devein/00_LAYNII_commands.sh
@@ -3,9 +3,10 @@
 # -M_brain_activity.nii.gz
 
 # Generate layers
-./LN2_LAYERS -rim /home/faruk/gdrive/LAYNII/demo_big3/M_brain_rim.nii.gz
+./LN2_LAYERS -rim /home/faruk/gdrive/LAYNII/demo_big3/M_brain_rim.nii.gz -nr_layers 5
 
 # Generate columns
 ./LN2_COLUMNS -rim ./LN2_LAYERS -rim /home/faruk/gdrive/LAYNII/demo_big3/M_brain_rim.nii.gz -midgm /home/faruk/gdrive/LAYNII/demo_big3/M_brain_rim_midGM_equidist.nii.gz -nr_columns 33
 
 # Devein
+./LN2_DEVEIN -input /home/faruk/gdrive/LAYNII/demo_big3/M_brain_draining_veins.nii.gz -layer_file /home/faruk/gdrive/LAYNII/demo_big3/M_brain_rim_layers_equidist.nii.gz -linear
