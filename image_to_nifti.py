64b3001e94a31d4930392e04d842dd7e17026477
Omer Faruk Gulban <farukgulban@gmail.com>
2020-11-16 18:01:11 +0100

    Add transpose line

--- image_to_nifti.py
+++ image_to_nifti.py
@@ -8,9 +8,9 @@ from scipy.ndimage import zoom
 
 # =============================================================================
 # Parameters
-PNG_FILE = "/home/faruk/avocado_toast.png"
+PNG_FILE = "/path/to/image.png"
 
-DOWNSAMPLING_FACTOR = 1/8
+DOWNSAMPLING_FACTOR = 1/2
 
 # =============================================================================
 # Load png
@@ -21,6 +21,8 @@ data_in = np.mean(data_in, axis=-1)
 # Downsample
 data_out = zoom(data_in, DOWNSAMPLING_FACTOR, mode="nearest",
                 prefilter=False)
+# Transpose
+data_out = data_out[::-1, ::-1].T
 # Save as Nifti
 img = nb.Nifti1Image(data_out, affine=np.eye(4))
-nb.save(img, "{}.nii".format(os.path.splitext(PNG_FILE)[0]))
+nb.save(img, "{}.nii.gz".format(os.path.splitext(PNG_FILE)[0]))
