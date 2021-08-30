b6228d067cd75bb2850a982c2e2212d496961f15
Renzo <layerfMRI@gmail.com>
2021-08-30 13:43:12 +0200

    Update afni_LayerMe_timecourse.sh

--- EXTRACT_LAYERPROFILES/afni_LayerMe_timecourse.sh
+++ EXTRACT_LAYERPROFILES/afni_LayerMe_timecourse.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 #get mean value
-3dROIstats -mask lo_layers.nii.gz -1DRformat -quiet -nzmean lo_BOLD_intemp.nii.gz > layer_t.dat
+3dROIstats -mask lo_layers.nii.gz -1DRformat -quiet -nzmean lo_BOLD_intemp.nii.gz > layers_mean.dat
+#get standard deviation
+3dROIstats -mask lo_layers.nii.gz -1DRformat -quiet -sigma  lo_BOLD_intemp.nii.gz > layer_std.dat
 
 #columns are the layers 
 #rows are the time points. 
